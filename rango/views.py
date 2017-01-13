faba0f5446ef0506ad8addda48377f7c56f5e832
sosaje <jonathansaje@glaroam2-186-13.wireless.gla.ac.uk>
2017-01-13 13:14:25 +0000

    Adding the Exercises for 3
    Adding the about page with links

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says her there partner!")
+    return HttpResponse("Rango says hey there partner!"
+                        "<p><a href='http://127.0.0.1:8000/rango/about/'>About</a></p>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page"
+                        "<p><a href='http://127.0.0.1:8000/rango/'>Home</a></p>")
 
