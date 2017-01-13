dcb284e0000e27ea06a1204627797d68dfb104dc
sosaje <jonathansaje@glaroam2-186-13.wireless.gla.ac.uk>
2017-01-13 13:57:10 +0000

    Adding of templates and static media
    Also the addition of a gitignore, adds image to index

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!"
-                        "<p><a href='http://127.0.0.1:8000/rango/about/'>About</a></p>")
 
+    context_dict = {'boldmessage':"Cruncy, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+# Return a rendered response to send to the client.
+# We make use of the shortcut function to make our lives easier.
+# Note that the first parameter is the template we wish to use.
 def about(request):
     return HttpResponse("Rango says here is the about page"
-                        "<p><a href='http://127.0.0.1:8000/rango/'>Home</a></p>")
+                        "<p><a href='/rango/'>Home</a></p>")
 
