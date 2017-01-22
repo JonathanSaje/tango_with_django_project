d55d3a5aa02cd8f1666d354350368ac48d98f0c1
sosaje <jonathansaje@Jonathans-MacBook-Pro.local>
2017-01-22 16:10:35 +0000

    Updated index to show Cats
    Implemented views to show categories created ordered by views

--- rango/admin.py
+++ rango/admin.py
@@ -7,7 +7,7 @@ class PageAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'category', 'url')
 
-    
+
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+
 
 def index(request):
 
-    context_dict = {'boldmessage':"Cruncy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 # Return a rendered response to send to the client.
 # We make use of the shortcut function to make our lives easier.
 # Note that the first parameter is the template we wish to use.
+
+
 def about(request):
 
     context_dict = {'boldmessage':"This tutorial has been put together by Jonathan Saje"}
