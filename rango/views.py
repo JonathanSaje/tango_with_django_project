4b5a17cf092b40d8b5226673ad2ae94b0cc6581c
sosaje <jonathansaje@Jonathans-MacBook-Pro.local>
2017-02-05 18:34:38 +0000

    Test Cookie added
    ch10.4 complete, test cookie added to index page, check shell when move
    from index to about page

--- rango/views.py
+++ rango/views.py
@@ -65,6 +65,7 @@ def show_category(request, category_name_slug):
 
 def index(request):
 
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages':page_list}
@@ -77,6 +78,10 @@ def index(request):
 
 
 def about(request):
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
 
     context_dict = {'boldmessage':"This tutorial has been put together by Jonathan Saje"}
 
