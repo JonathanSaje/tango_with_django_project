b8a6803d82418a00aa42e3d09e351073369c2094
sosaje <jonathansaje@Jonathans-MacBook-Pro.local>
2017-02-06 01:02:15 +0000

    Session Data Cookie Added
    Ch10.6 complete, changed the cookie handler

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def add_category(request):
@@ -65,12 +66,16 @@ def show_category(request, category_name_slug):
 
 def index(request):
 
-    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages':page_list}
 
-    return render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
 
 # Return a rendered response to send to the client.
 # We make use of the shortcut function to make our lives easier.
@@ -167,6 +172,38 @@ def user_login(request):
         return render(request, 'rango/login.html', {})
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        #update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] =  visits
+
+
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html', {})
@@ -177,5 +214,7 @@ def user_logout(request):
     logout(request)
 
     return HttpResponseRedirect(reverse('index'))
+
+
 
 
